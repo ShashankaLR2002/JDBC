6ecae5df3bbb922815ae8df3f823975de10df835
ShashankaLR2002 <shashankalr.xworkz@gmail.com>
2024-10-24 00:04:11 +0530

    JDBC with SQL

---
+++ JDBCConnection/src/com/xworkz/jdbccon/Train.java
@@ -0,0 +1,32 @@
+package com.xworkz.jdbccon;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Train {
+
+	public static void main(String[] args) {
+
+		String url = "jdbc:mysql://localhost:3306/fOREST";
+		String userName = "root";
+		String password = "Shash_123";
+
+		Connection connection = null;
+
+		try {
+			connection = DriverManager.getConnection(url, userName, password);
+
+			if (connection != null) {
+				System.out.println("Successfully Connected");
+
+			} else {
+				System.out.println("Not connected");
+			}
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+	}
+
+}
